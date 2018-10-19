1696f97f6cdcfcbf41c2899a03ffb65200594ec1
suhov-as <suhov.a.s@yandex.ru>
2018-10-19 17:19:41 +0300

    счетчик корзины

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,22 @@ $(function () {
             addParam(name, '')
         }
     });
+
+    $(document).on('click', '.cart-service', function () {
+        var button = $(this);
+        var price = parseInt(button.parent().find('.service-price').text(), 10);
+        var name = button.parent().find('.service-name').text();
+        var sumContainer = $('#totalsum');
+        var id = button.data('id');
+        if (button.prop('checked')) {
+            var string = '<li class="service-buy" data-id="' + id + '">Услуга ' + name + ' на сумму <span class="service-sum" data-id="' + id + '">' + price + '</span></li>';
+            $('#items-to-buy').append(string);
+            sumContainer.text(parseInt(sumContainer.text(), 10) + price);
+        } else {
+            $('#items-to-buy .service-buy[data-id="' + id + '"]').remove();
+            sumContainer.text(parseInt(sumContainer.text(), 10) - price);
+        }
+    });
 });
 
 function addParam(paramName, paramVal) {
@@ -146,6 +162,48 @@ function toCart(id) {
     sendRequest('/cart/to-cart', {id: id, cart: cartId}).then(data => f(data));
 }
 
+function plusAlbum(item_id) {
+    var sum = parseInt($('#totalsum').text());
+    var lotPrice = parseInt($('.price .price-num[data-id='+item_id+']').text());
+
+    function f(data) {
+        if (data.status === 'success') {
+            $('.album-num[data-id='+item_id+']').text(data.count);
+            $('.album-num[data-id='+item_id+']').prop('value', data.count);
+            $('.price-num[data-id='+item_id+']').text(data.price);
+            $('#totalsum').text(sum-lotPrice+data.price);
+        }
+    }
+    sendRequest('/cart/plus', {id: item_id, guid: cartId}).then(data => f(data));
+}
+
+function minusAlbum(item_id) {
+    var sum = parseInt($('#totalsum').text());
+    var lotPrice = parseInt($('.price .price-num[data-id='+item_id+']').text());
+    function f(data) {
+        if (data.status === 'success') {
+            $('.album-num[data-id='+item_id+']').text(data.count);
+            $('.album-num[data-id='+item_id+']').prop('value', data.count);
+            $('.price-num[data-id='+item_id+']').text(data.price);
+            $('#totalsum').text(sum-lotPrice+data.price);
+        }
+    }
+    sendRequest('/cart/minus', {id: item_id, guid: cartId}).then(data => f(data));
+}
+
+function deleteItem(item_id) {
+    var sum = parseInt($('#totalsum').text());
+    var lotPrice = parseInt($('.price-num[data-id='+item_id+']').text());
+    function f(data) {
+        if (data.status === 'success') {
+            $('.compare-table--item[data-id='+item_id+']').remove();
+            $('.you-buy[data-id='+item_id+']').remove();
+            $('#api-cart-count').text(parseInt($('#api-cart-count').text())-1);
+            $('#totalsum').text(sum-lotPrice);
+        }
+    }
+    sendRequest('/cart/delete-item', {id: item_id, guid: cartId}).then(data => f(data));
+}
 
 $(document).on('mouseup', '.range', function () {
     addParams({'minarea': $('#input-with-keypress-0').val(), 'maxarea': $('#input-with-keypress-1').val()});
